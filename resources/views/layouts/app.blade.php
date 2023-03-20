37ff4f94500e481244d92573b4a44f548251945e
Shehzad Khan <Shahzad.sk046@gmail.com>
2023-03-20 16:31:08 +0500

    Navbar and Footer Created

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'The Market Ladder') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <style>
+        .active {
+            color: green !important;
+            border-bottom: 2px solid green !important;
+        }
+    </style>
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'The Market Ladder') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        {{-- @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else --}}
+                        <li class="nav-item">
+                            <a class="nav-link @if (Route::currentRouteName() == 'home') active @endif"
+                                href="{{ route('home') }}">{{ __('HOME') }}</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ __('SWING TRADING FOR SUCCESS') }}
+                            </a>
+
+                            <div class="dropdown-menu bg-dark dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item text-white" href="{{ route('compounding-magic') }}">
+                                    {{ __('THE COMPOUNDING MAGIC') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('swing-trading-with-stocks') }}">
+                                    {{ __('SWING TRADING WITH STOCKS') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('swing-trading-with-etf') }}">
+                                    {{ __("SWING TRADING WITH ETF'S") }}
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ __('ABOUT THE MARKET LADDER') }}
+                            </a>
+
+                            <div class="dropdown-menu bg-dark dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item text-white" href="{{ route('about') }}">
+                                    {{ __('ABOUT THE MARKET LADDER') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('performance') }}">
+                                    {{ __('PERFORMANCE') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('about-founder') }}">
+                                    {{ __('ABOUT THE FOUNDER') }}
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ __('FYI') }}
+                            </a>
+
+                            <div class="dropdown-menu bg-dark dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item text-white" href="{{ route('faq') }}">
+                                    {{ __('FAQ') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('terms') }}">
+                                    {{ __('TERMS OF SERVICE') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('privacy-policy') }}">
+                                    {{ __('PRIVACY POLICY') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('contact') }}">
+                                    {{ __('CONTACT') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('disclaimers') }}">
+                                    {{ __('DISCLOSURES AND DISCLAIMERS') }}
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ __('MEMBERS ONLY') }}
+                            </a>
+
+                            <div class="dropdown-menu bg-dark dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item text-white" href="{{ route('post') }}">
+                                    {{ __("THE LADDER'S POST") }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('taking-trades') }}">
+                                    {{ __('TAKING THE TRADES') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('current-stock') }}">
+                                    {{ __('CURRENT STOCK TRADES') }}
+                                </a>
+                                <a class="dropdown-item text-white" href="{{ route('current-etf') }}">
+                                    {{ __('CURRENT ETF TRADES (FOR PLUS MEMBERS)') }}
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('join-now') }}">{{ __('JOIN NOW') }}</a>
+                        </li>
+                        {{-- <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu bg-dark dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item text-white" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li> --}}
+                        {{-- @endguest --}}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        <footer class="py-4">
+            <div class="container-fluid">
+                <div class="row pt-5 bg-dark text-white">
+                    <div class="col-md-3">
+                        LOGO
+                    </div>
+                    <div class="col-md-3">
+                        <h5>IT'S NEVER TOO LATE</h5>
+                        <p>Sign up to the market ladder today, and start your journey to stock market success!</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>CONTACT</h5>
+                        <p>Send your questions or comments to info@themarketladder.com</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>NOTICE</h5>
+                        <p>Stock Market Trades and Commentary shown by The Market Ladder, LLC, are for informational
+                            purposes only, and should not be considered Investment advice.</p>
+                    </div>
+                </div>
+                <div class="row pt-5 text-white bg-black opacity-75">
+                    <ul class="d-flex list-inline">
+                        <li><a href="/" class="navbar-brand"> Home </a></li> &nbsp;|&nbsp;
+                        <li><a href="/terms" class="navbar-brand"> Terms </a></li> &nbsp;|&nbsp;
+                        <li><a href="/disclosures" class="navbar-brand"> Disclosures </a></li>
+                        <li><a href="/privacy-policy" class="navbar-brand"> Privacy Policy </a></li> &nbsp;|&nbsp;
+                    </ul>
+                    <ul class="d-flex list-inline">
+                        <li>Website Design By Shehzad Khan</li> &nbsp;|&nbsp;
+                        <li>&copy; 2023 All Right Reserved</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::view('/compounding-magic','compounding-magic')->name('compounding-magic');
+Route::view('/swing-trading-with-stocks','swing-trading-with-stocks')->name('swing-trading-with-stocks');
+Route::view('/swing-trading-with-etf','swing-trading-with-etf')->name('swing-trading-with-etf');
+Route::view('/about','about')->name('about');
+Route::view('/performance','performance')->name('performance');
+Route::view('/about-founder','about-founder')->name('about-founder');
+Route::view('/faq','faq')->name('faq');
+Route::view('/terms','terms')->name('terms');
+Route::view('/privacy-policy','privacy-policy')->name('privacy-policy');
+Route::view('/contact','contact')->name('contact');
+Route::view('/disclaimers','disclaimers')->name('disclaimers');
+Route::view('/post','post')->name('post');
+Route::view('/taking-trades','taking-trades')->name('taking-trades');
+Route::view('/current-stock','current-stock')->name('current-stock');
+Route::view('/current-etf','current-etf')->name('current-etf');
+Route::view('/login','auth/login')->name('login');
+Route::view('/register','auth/register')->name('register');
+Route::view('/join-now','join-now')->name('join-now');
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
