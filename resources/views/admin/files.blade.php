c969d86e097ebd9767c308032e5c37856e3285c4
Shehzad Khan <Shahzad.sk046@gmail.com>
2023-03-30 17:18:17 +0500

    Files Page Completed

--- resources/views/admin/files.blade.php
+++ resources/views/admin/files.blade.php
@@ -6,4 +6,71 @@
             <li class="breadcrumb-item text-muted">Files</li>
         </ol>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-md-12">
+                        <button class="btn btns bg-green"><i class="fas fa-plus"></i> &nbsp; New Area</button>
+                    </div>
+                </div>
+                <div class="row my-3 py-2">
+                    {{-- <div class="col-12">
+                        <form class="d-none d-md-inline-block form-inline ms-auto me-0 my-2 my-md-0">
+                            <div class="input-group">
+                                <input class="form-control text-muted"
+                                       type="text"
+                                       placeholder="Search"
+                                       aria-label="Search"
+                                       aria-describedby="btnNavbarSearch" />
+                            </div>
+                        </form>
+                        <button class="btn btns bg-green"><i class="fa-solid fa-filter"></i> Filter</button>
+                        <button class="btn btns bg-gray">Clear</button>
+                    </div> --}}
+                </div>
+                <div class="row my-3 py-2">
+                    <div class="col-12">
+                        <table id="example" class="bg-white">
+                            <thead>
+                                <tr>
+                                    <th><input type="checkbox"
+                                               name="all_data"
+                                               id="all_data"></th>
+                                    <th>Preview</th>
+                                    <th>Name</th>
+                                    <th>Size</th>
+                                    <th>MIME Type</th>
+                                    <th>Folder</th>
+                                    <th>Edit</th>
+                                    <th>View</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><input type="checkbox"
+                                               name="all_data"
+                                               id="all_data"></td>
+                                    <td><img src="images/logo.png" alt="logo"></td>
+                                    <td>filename</td>
+                                    <td>file size</td>
+                                    <td>file type</td>
+                                    <td>images</td>
+                                    <td><button class="btn btns bg-red"
+                                                style="width: 100px"><i class="fas fa-pen"></i> Edit</button></td>
+                                    <td><button class="btn btns bg-lightblue"
+                                                style="width: 100px"><i class="fas fa-eye"></i>
+                                            View</button></td>
+                                </tr>
+                                <tr>
+                                    <td><button class="btn btns bg-pink"
+                                                style="width: 100px"><i class="fas fa-trash"></i> Delete</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/pages.blade.php
+++ resources/views/admin/pages.blade.php
@@ -6,4 +6,68 @@
             <li class="breadcrumb-item text-muted">Pages</li>
         </ol>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-md-12">
+                        <button class="btn btns bg-green"><i class="fas fa-plus"></i> &nbsp; New Area</button>
+                    </div>
+                </div>
+                <div class="row my-3 py-2">
+                    {{-- <div class="col-12">
+                        <form class="d-none d-md-inline-block form-inline ms-auto me-0 my-2 my-md-0">
+                            <div class="input-group">
+                                <input class="form-control text-muted"
+                                       type="text"
+                                       placeholder="Search"
+                                       aria-label="Search"
+                                       aria-describedby="btnNavbarSearch" />
+                            </div>
+                        </form>
+                        <button class="btn btns bg-green"><i class="fa-solid fa-filter"></i> Filter</button>
+                        <button class="btn btns bg-gray">Clear</button>
+                    </div> --}}
+                </div>
+                <div class="row my-3 py-2">
+                    <div class="col-12">
+                        <table id="example" class="bg-white">
+                            <thead>
+                                <tr>
+                                    <th><input type="checkbox"
+                                               name="all_data"
+                                               id="all_data"></th>
+                                    <th>Name</th>
+                                    <th>Slug</th>
+                                    <th>Description</th>
+                                    <th>Edit</th>
+                                    <th>Blocks</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><input type="checkbox"
+                                               name="all_data"
+                                               id="all_data"></td>
+                                    <td>Balloon Banner</td>
+                                    <td>balloon-banner</td>
+                                    <td>This banner is used in end of all internal Pages, so changes made here will be
+                                        reflected on every internal page.</td>
+                                    <td><button class="btn btns bg-red"
+                                                style="width: 100px"><i class="fas fa-pen"></i> Edit</button></td>
+                                    <td><button class="btn btns bg-purple"
+                                                style="width: 100px"><i class="fas fa-wand-magic-sparkles"></i>
+                                            Blocks</button></td>
+                                </tr>
+                                <tr>
+                                    <td><button class="btn btns bg-pink"
+                                                style="width: 100px"><i class="fas fa-trash"></i> Delete</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
