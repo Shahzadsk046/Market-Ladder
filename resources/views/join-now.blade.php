3dc9d73bed6fec0c80b65320e35e1b86282dae93
Shehzad Khan <Shahzad.sk046@gmail.com>
2023-03-30 11:29:50 +0500

    links added to anchor tag and some design changes

--- resources/views/join-now.blade.php
+++ resources/views/join-now.blade.php
@@ -1,9 +1,12 @@
 <head>
-    <link rel="stylesheet" href={{ asset('css/join-now.css') }}>
+    <link rel="stylesheet"
+          href={{ asset('css/join-now.css') }}>
 </head>
 @extends('layouts.app')
 
-@section('title') {{'Join Now'}} @endsection
+@section('title')
+    {{ 'Join Now' }}
+@endsection
 
 @section('content')
     <div class="join-now-page">
@@ -17,8 +20,8 @@
                 <div class="row my-3 py-5">
                     <table class="membership-table table table-bordered text-center lh-base">
                         <tr class="text-white">
-                            <th style="background-color:rgb(255,146,72)">BASIC PLAN</th>
-                            <th style="background-color: rgb(4, 70, 131)">PLUS PLAN</th>
+                            <th style="background-color:#fc630a91">BASIC PLAN</th>
+                            <th style="background-color: rgba(0,41,95,.685)">PLUS PLAN</th>
                         </tr>
                         <tr>
                             <td>Stock Trades Posted Regularly</td>
@@ -30,38 +33,33 @@
                         </tr>
                         <tr>
                             <td><strong>>>> *Monthly Plans starting at
-                                    <span class="text-success">just $22/month</span></strong>
+                                    <a href="/monthly-signup"
+                                       class="text-green">just $22/month</a></strong>
                                 <br><br>
-                                <a href="#" class="text-success text-decoration-none">Click</a>
+                                <a href="/monthly-signup"
+                                   class="text-green">Click</a>
                                 to start your Monthly Subscription
                                 <br><br>
-                                <strong><span class="bg-warning">>>> Or Get 2 months free!</span></strong> ... *By signing
-                                up
-                                for an Annual Membership. <a href="#"
-                                    class="text-success text-decoration-none">Click</a> to get your annual
-                                subscription of the Basic Plan at $220/year
+                                <strong><span class="bg-warning">>>> Or Get 2 months free!</span></strong> ... *By signing up for an Annual Membership. <a href="/annual-signup" class="text-green">Click</a> to get your annual subscription of the Basic Plan at $220/year
                             </td>
                             <td><strong>>>> *Monthly Plans starting at
-                                    <span class="text-success">just $32/month</span></strong>
+                                    <a href="/monthly-signup" class="text-green">just $32/month</a></strong>
                                 <br><br>
-                                <a href="#" class="text-success text-decoration-none">Click</a>
-                                to start your Monthly Subscription
+                                <a href="/monthly-signup" class="text-green">Click</a> to start your Monthly Subscription
                                 <br><br>
-                                <strong><span class="bg-warning">>>> Or Get 2 months free!</span></strong> ... *By signing
-                                up
-                                for an Annual Subscription. <a href="#"
-                                    class="text-success text-decoration-none">Click</a> to get your annual
-                                Subscription of the Plus Plan at $320/year
+                                <strong><span class="bg-warning">>>> Or Get 2 months free!</span></strong> ... *By signing up for an Annual Subscription. <a href="/annual-signup" class="text-green">Click</a> to get your annual Subscription of the Plus Plan at $320/year
                             </td>
                         </tr>
                     </table>
                     <p><strong>*Your Credit card will be billed automatically at the end of every Month/Year until you
                             cancel. Sorry, no refunds on Monthly Subscriptios, Annual subscriptions are allowed a prorated
                             refund for all full months remaining in the subscriber's account.</strong></p>
-                    <div class="border text-center mt-5">
+                    <div class="border text-center mt-5 p-3">
                         <p>First time Subscriber?.. Get a 30 day Free Trial of the Plus Plan, so you can try before you buy.
                         </p>
-                        <a href="#" class="text-decoration-none text-success">CLICK HERE FOR YOUR 30 DAY FREE
+                        <a href="/trial-signup"
+                           class="text-green fw-bold" style=" font-size: 11px; padding-right: 2vw;padding-left: 2vw; padding-top: 10px;padding-bottom: 10px;
+                           ">CLICK HERE FOR YOUR 30 DAY FREE
                             TRIAL</a>
                     </div>
                 </div>
